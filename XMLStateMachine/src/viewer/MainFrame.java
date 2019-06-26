ea20196e3c78faa7ee93241bbb6e00bce4a5c1ea
dusan <ducom98@gmail.com>
2019-06-26 17:08:21 +0200

    Dodat prikaz polja u MainView klasi

--- XMLStateMachine/src/viewer/MainFrame.java
+++ XMLStateMachine/src/viewer/MainFrame.java
@@ -14,7 +14,7 @@ public class MainFrame extends JFrame{
 	public MainFrame() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle(APP_TITLE);
-		setSize(500, 700);
+		setSize(400, 450);
 		setLocationRelativeTo(null);
 		
 		MainView mainView = new MainView();
