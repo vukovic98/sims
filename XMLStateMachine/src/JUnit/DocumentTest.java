f5d602d9094b356051e229aeba3ecff236a85aa3
vladimir <vladimirvukovic98@hotmail.rs>
2019-06-28 14:21:55 +0200

    JUnit paket

---
+++ XMLStateMachine/src/JUnit/DocumentTest.java
@@ -0,0 +1,19 @@
+package JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class DocumentTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
